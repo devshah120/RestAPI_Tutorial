1e7ff317ef4ec1889b2eda4101127e721e04cc2b
Dev Shah <devshah120902@gmail.com>
2023-02-08 16:46:55 +0530

    Added Error Handler

--- app.js
+++ app.js
@@ -3,7 +3,22 @@ const ProductsRoute = require("./routes/products.route");
 const app = express();
 app.use("/Products", ProductsRoute);
 
+app.use((req, res, next) => {
+  const err = new Error("Page Not Found");
+  err.status=404;
+  next(err);
+});
+
+//Error Handler
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send({
+    error: {
+      status: err.status || 500,
+      message: err.message,
+    },
+  });
+});
 app.listen(3000, () => {
   console.log("This is runnig on port 3000");
 });
-
